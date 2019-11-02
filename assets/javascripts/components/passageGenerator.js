633c6b616050ab07a2f678654d4723ca0e7ec59a
Matt Farmer <mfarmer@d120.org>
2019-11-02 15:41:17 +0000

    alert if fewer than 2 rhythms are selected

--- assets/javascripts/components/passageGenerator.js
+++ assets/javascripts/components/passageGenerator.js
@@ -15,22 +15,27 @@ const passageGenerator = function(blocks){
     this.generate = function(){
         this.np.reset();
         this.refresh();
-        //while the passage isn't full
-        let rhy, notes, beats;
-        while(this.beatsRemaining() > 0){
-            rhy = this.chooseRhythm();//select a rhythm
-            notes = notesFromString(rhy);//turn it into notes
-            beats = this.np.notesToBeats(notes,4);//calculate the length of those notes in beats
-            if(beats <= this.beatsRemaining()){//if adding to the passage won't over fill it, 
-                notes.forEach((n)=>{
-                    let targetNoteArray = ((this.beatsRemaining()<=(this.beatLength/2)) ? this.np.notes : this.np.notes2 );
-                    targetNoteArray.push(n);
-                    if(this.beatsRemaining()%4===0 && this.beatsRemaining()%16!==0){
-                        let bar = new VF.BarNote(VF.Barline.type.SINGLE);
-                        targetNoteArray.push(bar);
-                    }
-                });
-                
+        if(this.rhythmOptions.length<2){
+            alert("Please select 2 or more rhythm blocks to generate a rhythm passage.");
+        } else {
+            
+            //while the passage isn't full
+            let rhy, notes, beats;
+            while(this.beatsRemaining() > 0){
+                rhy = this.chooseRhythm();//select a rhythm
+                notes = notesFromString(rhy);//turn it into notes
+                beats = this.np.notesToBeats(notes,4);//calculate the length of those notes in beats
+                if(beats <= this.beatsRemaining()){//if adding to the passage won't over fill it, 
+                    notes.forEach((n)=>{
+                        let targetNoteArray = ((this.beatsRemaining()<=(this.beatLength/2)) ? this.np.notes : this.np.notes2 );
+                        targetNoteArray.push(n);
+                        if(this.beatsRemaining()%4===0 && this.beatsRemaining()%16!==0){
+                            let bar = new VF.BarNote(VF.Barline.type.SINGLE);
+                            targetNoteArray.push(bar);
+                        }
+                    });
+                    
+                }
             }
         }
         this.np.render();
