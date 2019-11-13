cd04c4cfda0e9d029ba6b3752df1fc2b11437aca
Matt Farmer <mfarmer@d120.org>
2019-11-13 02:16:11 +0000

    fixing blocks flexing

--- assets/javascripts/components/difficultyButtons.js
+++ assets/javascripts/components/difficultyButtons.js
@@ -57,16 +57,7 @@ const renderDifficultyButtons = function(difficulties, targetEl, selectedDifficu
     const optsContainer = document.getElementById("option-buttons-container");
     optsContainer.innerHTML="";
     let restButton = createRestButton(restsOn);
-    
-    // let infoButton = document.createElement('button');
-    // infoButton.setAttribute("id","myButton")
-    // infoButton.innerHTML = "Info"
-    // infoButton.className = "item difficulty-button";
-    // infoButton.addEventListener('click', function(){
-    //   MicroModal.show('modal-1');    
-    // });
     optsContainer.appendChild(restButton);
-    //optsContainer.appendChild(infoButton);
 };
 
 
